bae5b771ceea0713803fe6c2200c4bee97df493f
thomascardon35 <37211499+thomascardon35@users.noreply.github.com>
2018-05-07 23:37:07 +0200

    Add files via upload

--- projet/js/script.js
+++ projet/js/script.js
@@ -9,7 +9,7 @@ function onClickBtnDisplayNav(){
 
 function onClickBodyRemoveNav(event){
     //Si le clic s'est produit en dehors du bouton et de la nav
-    if(!$(event.target).closest('.div-nav-btn span, nav').length) {  
+    if(!$(event.target).closest('.div-nav-btn span').length) {  
 
         $('.nav-header').removeClass("width-nav"); // Alors on fait disparaitre la nav
         $('.fa-bars').removeClass("rotate-nav-btn"); // et on fait une rotation du bouton
@@ -19,22 +19,26 @@ function onClickBodyRemoveNav(event){
 
 
 function onScroll(){
+    var bubble4 = $(#bubble4);
+    var bubble5 = $('#bubble5');
+
+    var scrolltop = window.pageYOffset // get number of pixels document has scrolled vertically
+    bubble4.style.top = -scrolltop * .2 + 'px' // move bubble1 at 20% of scroll rate
+    bubble5.style.top = -scrolltop * .5 + 'px'
 
     // On stocke dans une variable la position verticale en px
     var windowTop = $(this).scrollTop();
 
     // Calcul de la hauteur maximum qu'un point fixe peut parcourir verticalement 
     var height = $(document).height()-$(window).height();
-
     // Récupération de la largeur de la fenêtre
     var width = $(window).width();
-
     // Calcul de la largeur de la barre		
     var bar = windowTop / height * width;
     
     // Modification du css en fonction de la position sur la page	
     $("#progressbar").css("width",bar);
-    
+
 
     // si le haut de la fenetre atteint la hauteur du header + 200px alors
     if(windowTop > $('header').height()+200){  
