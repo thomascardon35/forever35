3df4510e91424d645cec4e868c65b3f48728ed57
thomascardon35 <37211499+thomascardon35@users.noreply.github.com>
2018-05-17 15:34:34 +0200

    Add files via upload

--- projet/js/script.js
+++ projet/js/script.js
@@ -1,10 +1,10 @@
-"use strict"
+"use strict";
 
 function onClickBtnDisplayNav(){
     //$('.nav-header').slideToggle(500);
     $('.nav-header').toggleClass("width-nav"); // fait apparaitre/disparaitre la nav
     $('.fa-bars').toggleClass("rotate-nav-btn"); // rotation de l'icone ".fa-bars"
-}
+};
 
 
 function onClickBodyRemoveNav(event){
@@ -13,21 +13,52 @@ function onClickBodyRemoveNav(event){
 
         $('.nav-header').removeClass("width-nav"); // Alors on fait disparaitre la nav
         $('.fa-bars').removeClass("rotate-nav-btn"); // et on fait une rotation du bouton
-    } 
+    };
 };
 
+/* function onClickDescription(event){
+    event.preventDefault();
+    $('.description').toggleClass("hidden");
+} */
 
 
 function onScroll(){
-    var bubble4 = $(#bubble4);
-    var bubble5 = $('#bubble5');
-
-    var scrolltop = window.pageYOffset // get number of pixels document has scrolled vertically
-    bubble4.style.top = -scrolltop * .2 + 'px' // move bubble1 at 20% of scroll rate
-    bubble5.style.top = -scrolltop * .5 + 'px'
 
     // On stocke dans une variable la position verticale en px
     var windowTop = $(this).scrollTop();
+
+/*******************************************************************************
+*                                                                              *
+*                        Background Animé Bulles                               *
+*                                                                              *
+*******************************************************************************/
+    //si la largeur de l'écran utilisateur égale ou dépasse les 1025px(comme le mediaquerry) alors on anime les bulles en fond d'écran
+    if (window.matchMedia("(min-width: 1025px)").matches){
+        var bubble1 = $('#bubble1');
+        var bubble2 = $('#bubble2');
+        var bubble3 = $('#bubble3');
+        var bubble4 = $('#bubble4');
+        var bubble5 = $('#bubble5');
+        var bubble6 = $('#bubble6');
+        var bubble7 = $('#bubble7');
+        var bubble8 = $('#bubble8');
+        var bubble9 = $('#bubble9');
+
+        bubble1.css('top', windowTop * .15 + 'px'); 
+        bubble2.css('top', -windowTop * .05 + 'px'); 
+        bubble3.css('top', -windowTop * .25 + 'px');
+        bubble4.css('top', -windowTop * .15 + 'px');
+        bubble5.css('top', -windowTop * .35 + 'px');
+        bubble6.css('top', windowTop * .25 + 'px');
+        bubble7.css('top',windowTop * .05 + 'px');
+        bubble8.css('top', -windowTop * .05 + 'px'); 
+        bubble9.css('top', windowTop * .2 + 'px'); 
+    };
+/*******************************************************************************
+*                                                                              *
+*                     Barre de Progression en haut du site                     *
+*                                                                              *
+*******************************************************************************/
 
     // Calcul de la hauteur maximum qu'un point fixe peut parcourir verticalement 
     var height = $(document).height()-$(window).height();
@@ -35,22 +66,21 @@ function onScroll(){
     var width = $(window).width();
     // Calcul de la largeur de la barre		
     var bar = windowTop / height * width;
-    
     // Modification du css en fonction de la position sur la page	
     $("#progressbar").css("width",bar);
 
+/*******************************************************************************
+*                                                                              *
+*      Affichage Nav et Fleche de remontée du doc en fonction du scroll        *
+*                                                                              *
+*******************************************************************************/
 
     // si le haut de la fenetre atteint la hauteur du header + 200px alors
     if(windowTop > $('header').height()+200){  
-        $('main .arrow-up').removeClass('hidden');  // on fait apparaitre la fleche qui remonte en haut
+        $('main .arrow-up').removeClass('hidden');  // on fait apparaitre la fleche de remontée du document
         $('.nav-header').removeClass("width-nav");  // on fait disparaitre la nav
         $('.fa-bars').removeClass("rotate-nav-btn");  // et on fait une rotation du bouton
     }else{
-        $('main .arrow-up').addClass('hidden')  // sinon on fait disparaitre la fleche
-    }
-}
-
-/* function onClickDescription(event){
-    event.preventDefault();
-    $('.description').toggleClass("hidden");
-} */
+        $('main .arrow-up').addClass('hidden');  // sinon on fait disparaitre la fleche de remontée du document
+    };
+};
