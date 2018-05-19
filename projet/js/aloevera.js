82ece1dc2940c1fe55f75ad7c68c948767f1e8c2
thomascardon35 <37211499+thomascardon35@users.noreply.github.com>
2018-05-19 23:47:17 +0200

    Add files via upload

--- projet/js/aloevera.js
+++ projet/js/aloevera.js
@@ -24,11 +24,11 @@ $(window).scroll(function(){
 
 
     //Ici, en fonction de la hauteur de l'écran utilisateur on modifie la valeur que l'on va soustraire afin de faire apparaitre/disparaitre les éléments sur la page.
-    if (window.matchMedia("(max-height: 1000px)").matches){
+    if (window.matchMedia("(max-height: 950px)").matches){
         winHghtPctage   = ($(window).height()) * .37; 
-    } else if(window.matchMedia("(max-height: 1270px)").matches){
+    } else if(window.matchMedia("(max-height: 1200px)").matches){
         winHghtPctage   = ($(window).height()) * .5; 
-    } else if(window.matchMedia("(max-height: 1750px)").matches){
+    } else if(window.matchMedia("(max-height: 1720px)").matches){
         winHghtPctage   = ($(window).height()) * .65; 
     } else if(window.matchMedia("(max-height: 100000px)").matches){
         for ( index = 0; index < reasons.length; index++) { 
