9a56cbcb61670c26f3f0b155703aff1013e962dd
thomascardon35 <37211499+thomascardon35@users.noreply.github.com>
2018-05-21 22:06:55 +0200

    Add files via upload

--- projet/index.php
+++ projet/index.php
@@ -4,7 +4,7 @@
 include 'config/database.php';
 
 
-$sql = 'SELECT appointementdate ,city ,description,eventtype
+$sql = 'SELECT DAY(appointementdate)AS day,MONTH(appointementdate)AS month,YEAR(appointementdate)AS year,HOUR(appointementdate)AS hour,MINUTE(appointementdate)AS minute,city ,description,eventtype
         FROM newsevents' ;
 
 
@@ -12,7 +12,9 @@ $ressource = $pdo->prepare($sql);
 $ressource->execute();
 $newsevents = $ressource->fetchAll(PDO::FETCH_ASSOC);
 
-
+/* <?php foreach($newsevents as $newsevent): ?>
+                <p><?= $newsevent['description']?></p>
+            <?php endforeach; ?> */
 
 
 
