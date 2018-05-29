44dd87dc902718f5d52040a1c4e9948772df484d
thomascardon35 <37211499+thomascardon35@users.noreply.github.com>
2018-05-29 19:26:12 +0200

    Add files via upload

--- projet/index.php
+++ projet/index.php
@@ -1,10 +1,9 @@
 <?php
 
+include 'models/newsEventsModel.php';
 
-/* include 'config/database.php';
 
-
-$sql = 'SELECT DAY(appointementdate)AS day,MONTH(appointementdate)AS month,YEAR(appointementdate)AS year,HOUR(appointementdate)AS hour,MINUTE(appointementdate)AS minute,city ,eventdescription,eventtype
+/*$sql = 'SELECT DAY(appointementdate)AS day,MONTH(appointementdate)AS month,YEAR(appointementdate)AS year,HOUR(appointementdate)AS hour,MINUTE(appointementdate)AS minute,city ,eventdescription,eventtype
         FROM newsevents' ;
 
 
@@ -12,19 +11,10 @@ $ressource = $pdo->prepare($sql);
 $ressource->execute();
 $newsevents = $ressource->fetchAll(PDO::FETCH_ASSOC); */
 
-/* <?php foreach($newsevents as $newsevent): ?>
-            <ul>
-                <li>Le <?= $newsevent['day'] . '/' . $newsevent['month']. '/' . $newsevent['year']?> à <?= $newsevent['hour'] . 'h' . $newsevent['minute']?> </li>
-                <li class="<?= $newsevent['city']?>">A <?= substr($newsevent['city'],0,2);?></li>
-                <li><?= $newsevent['eventdescription']?></li>
-            </ul>
-        <?php endforeach; ?> */
+$newsEventsModel = new newsEventsModel;
+$newsEvents= $newsEventsModel->read();
 
-/* <?php foreach($newsevents as $newsevent): ?>
-                <p><?= $newsevent['description']?></p>
-            <?php endforeach; ?> */
-
-
+var_dump($newsEventsModel->read());
 
 $template="view/home";
 include "view/layout.phtml";
