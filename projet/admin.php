798f525f09886b58fbf2d35660f68f32ca64f2b6
thomascardon35 <37211499+thomascardon35@users.noreply.github.com>
2018-05-30 18:15:58 +0200

    Add files via upload

--- projet/admin.php
+++ projet/admin.php
@@ -1,14 +1,13 @@
 <?php
 
+require_once 'models/newsEventsModel.php';
 
+$newsEventsModel = new newsEventsModel;
+$newsEvents= $newsEventsModel->read();
 
-
-
-
-
-
-
-
+if(array_key_exists('delete_id', $_POST)){
+    //TODO
+}
 
 
 
--- projet/index.php
+++ projet/index.php
@@ -1,6 +1,11 @@
 <?php
 
-include 'models/newsEventsModel.php';
+require_once 'models/newsEventsModel.php';
+
+$newsEventsModel = new newsEventsModel;
+$newsEvents= $newsEventsModel->read();
+
+
 
 
 /*$sql = 'SELECT DAY(appointementdate)AS day,MONTH(appointementdate)AS month,YEAR(appointementdate)AS year,HOUR(appointementdate)AS hour,MINUTE(appointementdate)AS minute,city ,eventdescription,eventtype
@@ -11,10 +16,6 @@ $ressource = $pdo->prepare($sql);
 $ressource->execute();
 $newsevents = $ressource->fetchAll(PDO::FETCH_ASSOC); */
 
-$newsEventsModel = new newsEventsModel;
-$newsEvents= $newsEventsModel->read();
-
-var_dump($newsEventsModel->read());
 
 $template="view/home";
 include "view/layout.phtml";
