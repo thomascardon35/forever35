6f1548a3b0603c96a52414c9299dd0d80ea412a3
thomascardon35 <37211499+thomascardon35@users.noreply.github.com>
2018-05-30 18:13:05 +0200

    Add files via upload

--- projet/models/newsEventsModel.php
+++ projet/models/newsEventsModel.php
@@ -1,6 +1,6 @@
 <?php
 
-include 'config/database.php';
+require_once 'config/databaseClass.php';
 
 class newsEventsModel{
     private $db;
@@ -8,11 +8,37 @@ class newsEventsModel{
     function __construct(){
         $this->db = new Database;
     }
+
+    function create($appointementDate,$endTimeEvent,$city,$eventType,$eventDescription){
+        
+        if(empty($eventDescription)){
+            echo("Vous n'avez pas rempli le champs description");
+        }else{
+
+            $sql = 'INSERT INTO newsevents(appointementdate,endtimeevent,city,eventtype,eventdescription)
+                    VALUES (?,?,?,?,?)';
+                
+            $newsEvent = $this->db->executeSql($sql,[$appointementDate,$endTimeEvent,$city,$eventType,$eventDescription]);
+        }
+    }
     
     function read(){
-        $sql = 'SELECT DAY(appointementdate)AS day,MONTH(appointementdate)AS month,YEAR(appointementdate)AS year,HOUR(appointementdate)AS hour,MINUTE(appointementdate)AS minute,city ,eventdescription,eventtype
+        $sql = 'SELECT id, DATE_FORMAT(appointementdate,"%e %M %Y") AS eventdate, DATE_FORMAT(appointementdate,"%H:%i") AS eventtime,city ,eventdescription,eventtype
         FROM newsevents' ;
 
         return $this->db->query($sql);
     }
+
+    function delete($id){
+        $sql = 'DELETE FROM newsevents WHERE id=?';
+
+        $this->db->executeSql($sql,$id);
+    }
+
+    function update(){
+
+    }
 }
+
+
+/* DAY(appointementdate)AS day,MONTH(appointementdate)AS month,YEAR(appointementdate)AS year, HOUR(appointementdate)AS hour,MINUTE(appointementdate)AS minute */
