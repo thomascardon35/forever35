c225b658631defdc64b7e1bacaa9122bc81cecd3
thomascardon35 <37211499+thomascardon35@users.noreply.github.com>
2018-06-06 23:08:51 +0200

    Add files via upload

--- projet/admin.php
+++ projet/admin.php
@@ -2,11 +2,17 @@
 
 require_once 'models/newsEventsModel.php';
 
-$newsEventsModel = new newsEventsModel;
+$newsEventsModel = new NewsEvents;
 $newsEvents= $newsEventsModel->read();
 
-if(array_key_exists('delete_id', $_POST)){
-    //TODO
+
+
+if(array_key_exists('delete_id', $_GET)){  // ici on récupère l'information de la query string delete_id = le clique sur la poubelle
+
+    $newsEventsModel->delete([$_GET['delete_id']]); // on utlilise la méthode delete de newsEventsModel
+
+    header('Location: admin.php');
+    exit();
 }
 
 
