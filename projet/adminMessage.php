261d63709b370b88bd3a97554e63ff40e5f22c76
thomascardon35 <37211499+thomascardon35@users.noreply.github.com>
2018-06-11 23:30:24 +0200

    Add files via upload

--- projet/adminMessage.php
+++ projet/adminMessage.php
@@ -1,22 +1,31 @@
 <?php
+session_start();
+
+if (isset($_SESSION['login'])){//On vérifie que le variable existe.
+    $login=$_SESSION['login'];//On récupère la valeur de la variable de session.
+}else{
+    $login=0;//Si $_SESSION['connect'] n'existe pas, on donne la valeur "0".
+}
+
+
+if ($login == "1"){ // Si le visiteur s'est identifié, on affiche la page
 
 
 
+    if(array_key_exists('logout', $_POST)){
+        $_SESSION = [];
 
+        if (isset($_COOKIE[session_name()])){
+            setcookie(session_name(),'',time()-4200,'/');
+        }
 
+        session_destroy();
+    }
 
+    $template="view/adminMessage";
+    include "view/layout.phtml";
 
-
-
-
-
-
-
-
-
-
-
-
-
-$template="view/adminMessage";
-include "view/layout.phtml";
+}else{
+    header('Location: adminlogIn.php');
+    exit();
+}
