84e50046fd276343f4c4693f88f6e2c048400b57
thomascardon35 <37211499+thomascardon35@users.noreply.github.com>
2018-06-11 23:30:10 +0200

    Add files via upload

--- projet/admin.php
+++ projet/admin.php
@@ -1,27 +1,43 @@
 <?php
+session_start();
 
-require_once 'models/newsEventsModel.php';
-
-$newsEventsModel = new NewsEvents;
-$newsEvents= $newsEventsModel->read();
-
-
-
-if(array_key_exists('delete_id', $_GET)){  // ici on récupère l'information de la query string delete_id = le clique sur la poubelle
-
-    $newsEventsModel->delete([$_GET['delete_id']]); // on utlilise la méthode delete de newsEventsModel
-
-    header('Location: admin.php');
-    exit();
+if (isset($_SESSION['login'])){//On vérifie que le variable existe.
+    $login=$_SESSION['login'];//On récupère la valeur de la variable de session.
+}else{
+    $login=0;//Si $_SESSION['connect'] n'existe pas, on donne la valeur "0".
 }
 
 
+if ($login == "1"){ // Si le visiteur s'est identifié, on affiche la page
+
+    require_once 'models/newsEventsModel.php';
+
+    $newsEventsModel = new NewsEvents;
+    $newsEvents= $newsEventsModel->read();
 
 
+    if(array_key_exists('delete_id', $_GET)){  // ici on récupère l'information de la query string delete_id = le clique sur la poubelle
 
+        $newsEventsModel->delete([$_GET['delete_id']]); // on utlilise la méthode delete de newsEventsModel
 
+        header('Location: admin.php');
+        exit();
+    }
 
+    if(array_key_exists('logout', $_POST)){
+        $_SESSION = [];
 
+        if (isset($_COOKIE[session_name()])){
+            setcookie(session_name(),'',time()-4200,'/');
+        }
 
-$template="view/admin";
-include "view/layout.phtml";
+        session_destroy();
+    }
+
+    $template="view/admin";
+    include "view/layout.phtml";
+
+}else{
+    header('Location: adminlogIn.php');
+    exit();
+}
