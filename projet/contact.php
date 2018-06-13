0a912eed89a3e948501192d16e60a881b096c9fb
thomascardon35 <37211499+thomascardon35@users.noreply.github.com>
2018-06-14 01:17:20 +0200

    Add files via upload

--- projet/contact.php
+++ projet/contact.php
@@ -1,11 +1,37 @@
 <?php
 
 require_once 'models/newsEventsModel.php';
+require_once 'models/messageModel.php';
+
 
 $newsEventsModel = new NewsEvents;
 $newsEvents= $newsEventsModel->read();
 
 
+if(array_key_exists('submitMessage',$_POST)){
+
+    $messageModel = new Message;
+
+    if(empty($_POST['firstname']) || empty($_POST['lastname']) || empty($_POST['zipcode']) || empty($_POST['email']) || empty($_POST['subjectmessage']) || empty($_POST['usermessage'])){
+
+        echo("Vous n'avez pas rempli les champs obligatoires * ");
+
+    }else{
+
+        $gender             = ($_POST['gender']);
+        $firstName          = trim($_POST['firstname']);
+        $lastName           = trim($_POST['lastname']);
+        $zipCode            = trim($_POST['zipcode']);
+        $city               = trim($_POST['city']);
+        $phone              = trim($_POST['phone']);
+        $email              = trim($_POST['email']);
+        $subjectMessage     = $_POST['subjectmessage'];
+        $userMessage        = trim($_POST['usermessage']);
+
+
+        $messageModel->create($gender,$firstName,$lastName,$zipCode,$city,$phone,$email,$subjectMessage,$userMessage);
+    }
+}
 
 
 
