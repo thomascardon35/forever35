1f8c0918b4fa236ed5f2ad0958361c6189083874
thomascardon35 <37211499+thomascardon35@users.noreply.github.com>
2018-06-18 01:19:54 +0200

    Add files via upload

--- projet/models/messageModel.php
+++ projet/models/messageModel.php
@@ -7,14 +7,39 @@ class Message{
 
     function __construct(){
         $this->db = new Database;
+        $this->errorMessage = null;/* 
+        $this->formFields = array(); */
     }
 
+/*     function bind($formFields) {
+        foreach ($formFields as $name => $value) {
+            if (array_key_exists($name, $this->formFields) == true) {
+                $this->formFields[$name] = htmlentities($value);
+                
+            }
+        }
+    } */
+
     function create($gender,$firstName,$lastName,$zipCode,$city,$phone,$email,$subjectMessage,$userMessage){
-       
+        
+        if(empty($firstName) || empty($lastName) || empty($zipCode)  || empty($email)  || empty($subjectMessage) || empty($userMessage)) throw new DomainException("Tous les champs marqués d'un * doivent être remplis.");
+
+        if (!ctype_digit($zipCode) || strlen($zipCode) != 5) throw new DomainException("Le code postal doit contenir 5 chiffres.");
+
+        if($phone != null && strlen($phone) != 10) throw new DomainException("Le numéro de téléphone doit contenir 10 chiffres.");
+        
+        if ($phone != null){
+            if(!ctype_digit($phone)) throw new DomainException("Le numéro de téléphone ne doit contenir que des chiffres.");
+        }else{
+            $phone = 0;
+        }
+
+        if (!preg_match('/^(?!(?:(?:\x22?\x5C[\x00-\x7E]\x22?)|(?:\x22?[^\x5C\x22]\x22?)){255,})(?!(?:(?:\x22?\x5C[\x00-\x7E]\x22?)|(?:\x22?[^\x5C\x22]\x22?)){65,}@)(?:(?:[\x21\x23-\x27\x2A\x2B\x2D\x2F-\x39\x3D\x3F\x5E-\x7E]+)|(?:\x22(?:[\x01-\x08\x0B\x0C\x0E-\x1F\x21\x23-\x5B\x5D-\x7F]|(?:\x5C[\x00-\x7F]))*\x22))(?:\.(?:(?:[\x21\x23-\x27\x2A\x2B\x2D\x2F-\x39\x3D\x3F\x5E-\x7E]+)|(?:\x22(?:[\x01-\x08\x0B\x0C\x0E-\x1F\x21\x23-\x5B\x5D-\x7F]|(?:\x5C[\x00-\x7F]))*\x22)))*@(?:(?:(?!.*[^.]{64,})(?:(?:(?:xn--)?[a-z0-9]+(?:-[a-z0-9]+)*\.){1,126}){1,}(?:(?:[a-z][a-z0-9]*)|(?:(?:xn--)[a-z0-9]+))(?:-[a-z0-9]+)*)|(?:\[(?:(?:IPv6:(?:(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){7})|(?:(?!(?:.*[a-f0-9][:\]]){7,})(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,5})?::(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,5})?)))|(?:(?:IPv6:(?:(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){5}:)|(?:(?!(?:.*[a-f0-9]:){5,})(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,3})?::(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,3}:)?)))?(?:(?:25[0-5])|(?:2[0-4][0-9])|(?:1[0-9]{2})|(?:[1-9]?[0-9]))(?:\.(?:(?:25[0-5])|(?:2[0-4][0-9])|(?:1[0-9]{2})|(?:[1-9]?[0-9]))){3}))\]))$/iD' , $email)) throw new DomainException("Veuillez indiquer un E-mail valide .");
+
         $sql = 'INSERT INTO contactmessage(datemessage,gender,firstname,lastname,zipcode,city,phone,email,subjectmessage,usermessage)
                 VALUES (NOW(),?,?,?,?,?,?,?,?,?)';
                 
-        $newsEvent = $this->db->executeSql($sql,[$gender,$firstName,$lastName,$zipCode,$city,$phone,$email,$subjectMessage,$userMessage]);
+        $this->db->executeSql($sql,[$gender,$firstName,$lastName,$zipCode,$city,$phone,$email,$subjectMessage,$userMessage]);
         
     }
 
@@ -37,10 +62,17 @@ class Message{
         return $this->db->queryOne($sql,[$id]);
     }
 
-
     function delete($id){
         $sql = 'DELETE FROM contactmessage WHERE id=?';
 
         $this->db->executeSql($sql,[$id]);
     }
-}
+
+    function getErrorMessage() {
+        return $this->errorMessage;
+    }
+
+    function setErrorMessage($errorMessage) {
+        $this->errorMessage = $errorMessage;
+    }
+};
