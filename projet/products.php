0014b5c6d50161069bb7a66c0a52f52e80c9a7c1
thomascardon35 <37211499+thomascardon35@users.noreply.github.com>
2018-07-05 02:11:24 +0200

    Add files via upload

--- projet/products.php
+++ projet/products.php
@@ -2,10 +2,19 @@
 
 
 require_once 'models/newsEventsModel.php';
+require_once 'models/productModel.php';
 
 $newsEventsModel = new NewsEvents;
 $newsEvents= $newsEventsModel->read();
 
+$productModel = new Product;
+
+$newProducts            = $productModel->readByCategory('Nouveautés');
+$healthProducts         = $productModel->readByCategory('Santé');
+$painProducts           = $productModel->readByCategory('Douleur');
+$skinProducts           = $productModel->readByCategory('Peau');
+$dailyProducts          = $productModel->readByCategory('Quotidien');
+$houseAnimalsProducts   = $productModel->readByCategory('Maison Animaux');
 
 
 
